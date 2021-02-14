65e4c5d2d1e4d84e9fbf6d8f74f300415094f127
cosimo chellini <cosimo.chellini@gmail.com>
2021-02-14 12:13:07 +0100

    fix(dep): updated dependency

--- schemas/base/matchBaseFields.js
+++ schemas/base/matchBaseFields.js
@@ -7,20 +7,9 @@ export const baseMatch = [
     validation: (Rule) => Rule.required(),
   },
   {
-    title: "Created At",
-    name: "createdAt",
-    type: "date",
-    validation: (Rule) => Rule.required(),
-  },
-  {
     title: "Updated By",
     name: "updatedBy",
     type: "reference",
     to: { type: "player" },
-  },
-  {
-    title: "Updated At",
-    name: "updatedAt",
-    type: "date",
   },
 ];
